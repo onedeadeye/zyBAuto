1fb98251a7ff486bf59d6e394566bab852b60297
cbass <vcbass0@icloud.com>
2020-02-17 15:59:34 -0800

    wip
    pastes text answers into text box

--- zyBAuto.user.js
+++ zyBAuto.user.js
@@ -16,7 +16,7 @@ var autoRun = false;
 // Do not edit below this line!
 // ==========================================
 (function() {
-    console.log(timeString() + " [zBA] Begin zyBooks Autocomplete by Evanito.");
+    console.log(timeString() + " [zBA] Begin zyBooks Autocomplete by Evanito \n Edited by Cbass.");
     if (autoRun) {
         run();
     } else {
@@ -30,11 +30,17 @@ function zBAStartButton (zEvent) {
     console.log(timeString() + " [zBA] Running...");
     run();
 }
-
+var o = 0;
 function run() {
     //click_speeds();
     click_plays();
     click_starts();
+    console.log("loop");
+    if (o == 0){
+        textIn();
+    }
+    o= o +1;
+    //multChoice()
     setTimeout(function(){ run(); }, 1000);
 }
 
@@ -74,4 +80,28 @@ function timeString() {
     let s = (d.getSeconds()<10?'0':'') + d.getSeconds();
     let dstr = h + ':' + m + ":" + s;
     return dstr;
+}
+function textIn(){
+    var checker = document.getElementsByClassName("show-answer-button");
+    for (var i = 0; i < checker.length; i++) {
+        checker[i].click();
+        checker[i].click();
+        console.log(timeString() + " Clicked a get answer button.");
+        var targText = document.getElementsByClassName("forfeit-answer");
+        console.log(targText[i].textContent);
+        var paster = document.getElementsByClassName("zb-text-area");
+        paster[i].value = targText[i].textContent;
+    }
+}
+function multChoice(){
+    var question = document.getElementsByClassName("zb-radio-button");
+    var incorrect = document.getElementsByClassName("explanation");
+    console.log("test");
+    for (var b = 0; b < question.length; b++) {
+        if ((question[b].innerHTML).includes("Incorrect")) {
+            question[b].click();
+            console.log(timeString() + " Checked a q box.");
+        }
+
+    }
 }
