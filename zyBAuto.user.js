f737be4686e18842f54a771f9055f076b95e4c22
Sebastian <cbass87jk@gmail.com>
2020-04-22 19:26:21 -0700

    fixed text input and 2x speed

--- zyBAuto.user.js
+++ zyBAuto.user.js
@@ -15,50 +15,50 @@ var autoRun = false;
 
 // Do not edit below this line!
 // ==========================================
-(function() {
+(function () {
     console.log(timeString() + " [zBA] Begin zyBooks Autocomplete by Evanito, Edited By Cbass.");
     if (autoRun) {
         run();
     } else {
         document.getElementsByClassName('right-buttons')[0].innerHTML += '<button id="zbaButton" type="button">Autocomplete!</button>';
-        document.getElementById("zbaButton").addEventListener ("click", zBAStartButton, false);
+        document.getElementById("zbaButton").addEventListener("click", zBAStartButton, false);
     }
 })();
 
 
-function zBAStartButton (zEvent) {
+function zBAStartButton(zEvent) {
     console.log(timeString() + " [zBA] Running...");
     run();
-}
-var o = 0;
-function run() {
-    //click_speeds();
-    click_plays();
-    click_starts();
-    console.log("loop");
-    if (o == 0){
-        textIn();
-        multChoice();
-    }
-    o= o +1;
-    setTimeout(function(){ run(); }, 1000);
+    other();
 }
 
-function click_speeds() { // Checks speed boxes. Doesn't work but isn't a necessary feature.
+function run() {
+    click_speeds();
+    console.log("loop");
+    textIn();
+    multChoice();
+}
+function other() {
+    click_plays();
+    click_starts();
+    setTimeout(function () {
+        other();
+    }, 1000);
+}
+
+function click_speeds() { // Checks speed boxes.
     var speed = document.getElementsByClassName("speed-control");
     for (var i = 0; i < speed.length; i++) {
-        if ((speed[i].innerHTML).includes("false")) {
-            //speed[i].click();
-            speed[i].getElementsByClassName("zb-checkbox")[0].innerHTML = "\n<input type=\"checkbox\" value=\"true\" aria-label=\"2x speed\">\n<label aria-hidden=\"true\">2x speed</label>\n"
-            console.log(timeString() + " Checked a speed box.");
-        }
+        console.log(speed[i].firstElementChild);
+        speed[i].firstElementChild.firstChild.click();
+        console.log(" Checked a speed box.");
     }
 }
 
 function click_plays() { // Clicks all Play buttons
     var plays = document.getElementsByClassName("play-button");
     for (var i = 0; i < plays.length; i++) {
-        if (!(plays[i].classList).contains("rotate-180")){
+        if (!(plays[i].classList).contains("rotate-180")) {
             plays[i].click();
             console.log(timeString() + " Clicked a play button.");
         }
@@ -75,19 +75,20 @@ function click_starts() { // Clicks all Start buttons
 
 function timeString() {
     let d = new Date();
-    let h = (d.getHours()<10?'0':'') + d.getHours();
-    let m = (d.getMinutes()<10?'0':'') + d.getMinutes();
-    let s = (d.getSeconds()<10?'0':'') + d.getSeconds();
+    let h = (d.getHours() < 10 ? '0' : '') + d.getHours();
+    let m = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
+    let s = (d.getSeconds() < 10 ? '0' : '') + d.getSeconds();
     let dstr = h + ':' + m + ":" + s;
     return dstr;
 }
-function textIn(){
-    var qType = document.getElementsByClassName("question-set-question short-answer-question");
+
+function textIn() {
     var checker = document.getElementsByClassName("show-answer-button");
     var c = 0;
     var targText = document.getElementsByClassName("forfeit-answer");
+    var checkButton = document.getElementsByClassName("check-button");
     var paster = document.getElementsByClassName("zb-text-area");
-    console.log(checker);
+    console.log(checkButton);
     for (var i = 0; i < checker.length; i++) {
         checker[i].click();
         checker[i].click();
@@ -97,31 +98,39 @@ function textIn(){
         paster[i].value = targText[c].textContent;
         var hold = exp[i].getElementsByClassName("answers")[0];
         var skips = hold.getElementsByClassName("or-text").length;
-        if (skips!=0){
-            c=c+skips;
+        paster[i].focus();
+        if (skips != 0) {
+            c = c + skips;
             c++;
-        }
-        else{
+        } else {
             c++;
         }
     }
+    c = 0;
+    for (var k = 0; k < checkButton.length; k++) {
+        paster[k].focus();
+        checkButton[k].click();
+    }
+
 }
-function multChoice(){
+
+function multChoice() {
     var allQ = document.getElementsByClassName("question-set-question multiple-choice-question");
     var button = allQ[0].getElementsByClassName("zb-radio-button");
-    var incorrect = document.getElementsByClassName("explanation");
     console.log(button.length);
+    console.log(allQ.length);
     for (var b = 0; b < allQ.length; b++) {
-        if(allQ[b].innerHTML.includes("Question completed")){
-        }
-        else{
+        console.log(b);
+        if (allQ[b].innerHTML.includes("Question completed")) {
+        } else {
             var buttonQ = allQ[b].getElementsByClassName("zb-radio-button");
             console.log(buttonQ.length);
-            for(var c =0; c<buttonQ.length;c++){
+            for (var c = 0; c < buttonQ.length; c++) {
                 buttonQ[c].getElementsByTagName('input')[0].click();
             }
 
         }
     }
+
 
 }
